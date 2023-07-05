8605c090212c2f7f341fbd753f768d70d5300cb3
Ha989 <viethanguyen0809@gmail.com>
2023-07-05 23:24:46 +0700

    #fix earning chart

--- src/features/creator/EarningGraph.js
+++ src/features/creator/EarningGraph.js
@@ -1,10 +1,5 @@
-import React, { useCallback } from "react";
-import {
-  format,
-  startOfMonth,
-  endOfMonth,
-  eachMonthOfInterval,
-} from "date-fns";
+import React from "react";
+import { format } from "date-fns";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -13,8 +8,8 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
-import { Bar } from 'react-chartjs-2';
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
 import { useMediaQuery } from "@mui/material";
 import { useTheme } from "@emotion/react";
 
@@ -27,7 +22,6 @@ ChartJS.register(
   Legend
 );
 
-
 const colors = [
   "rgba(255, 99, 132, 0.8)",
   "rgba(54, 162, 235, 0.8)",
@@ -35,7 +29,7 @@ const colors = [
   "rgba(75, 192, 192, 0.8)",
   "rgba(153, 102, 255, 0.8)",
   "rgba(255, 159, 64, 0.8)",
-  "rgba(255, 99, 132, 0.8)",
+  "#117c24",
   "rgba(54, 162, 235, 0.8)",
   "rgba(255, 206, 86, 0.8)",
   "rgba(75, 192, 192, 0.8)",
@@ -45,81 +39,87 @@ const colors = [
 
 const EarningGraph = ({ projects }) => {
   const theme = useTheme();
-  const isXs = useMediaQuery(theme.breakpoints.only('xs'));
+  const isXs = useMediaQuery(theme.breakpoints.only("xs"));
 
-   // Calculate the monthly earnings based on the projects data
-   const calculateMonthlyEarnings = useCallback(() => {
-    const currentDate = new Date();
-    const currentMonthStart = startOfMonth(currentDate);
-    console.log("current", currentMonthStart)
-    const currentMonthEnd = endOfMonth(currentDate);
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
 
-    const months = eachMonthOfInterval({
-      start: startOfMonth(new Date(2023, 0)), // Set the desired start month
-      end: currentMonthEnd,
-    });
+  const months = Array.from(
+    { length: 12 },
+    (_, index) => new Date(currentYear, index, 1)
+  );
 
-    const monthlyEarnings = months.map((month) => {
-      const monthStart = startOfMonth(month);
-      const monthEnd = endOfMonth(month);
-      const earnings = projects?.reduce((total, project) => {
-        const projectDate = new Date(project.createdAt);
-        if (projectDate >= monthStart && projectDate <= monthEnd) {
-          return total + project.currentRaised;
-        }
-        return total;
-      }, 0);
-      return { month: month, earnings: earnings };
-    });
-    return monthlyEarnings;
-  }, [projects]);
+  const monthlyEarnings = months.map((month) => ({ month, earnings: 0 }));
 
+  // Calculate earnings for each project and update month's earnings
+  projects.forEach((project) => {
+    const projectDate = new Date(project.createdAt);
 
-      const monthlyEarnings = calculateMonthlyEarnings();
-      const labels = monthlyEarnings.map((item) =>
-        format(item.month, "MMM yyyy")
+    // Check if the project is within the current year
+    if (projectDate.getFullYear() === currentYear) {
+      const donations = project.donations || [];
+      const acceptDonations = donations.filter(
+        (donation) => donation.isConfirm === true
       );
-      const data = monthlyEarnings.map((item) => item.earnings);
+      acceptDonations.forEach((donation) => {
+        const donationDate = new Date(donation.createdAt);
 
-      const chartData = {
-        labels: labels,
-        datasets: [
-          {
-            label: "Monthly Earnings",
-            data: data,
-            backgroundColor: colors,
-          },
-        ],
-      };
+        // check if donation date greater or equal to current month
+        // check if donation date less than start date of next month
+        const monthIndex = months.findIndex(
+          (month) =>
+            donationDate >= month &&
+            donationDate <
+              new Date(month.getFullYear(), month.getMonth() + 1, 1)
+        );
+         // if matching month found
+        if (monthIndex !== -1) {
+          monthlyEarnings[monthIndex].earnings += donation.amount || 0;
+        }
+      });
+    }
+  });
 
-      const options = {
-        responsive: true,
-        plugins: {
-          legend: {
-            position: 'top',
-          },
-          title: {
-            display: true,
-            text: 'monthly earning of all projects',
-          },
+  const labels = monthlyEarnings.map((item) => format(item.month, "MMM yyyy"));
+  const data = monthlyEarnings.map((item) => item.earnings);
+  const chartData = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Monthly Earnings",
+        data: data,
+        backgroundColor: colors,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "monthly earning of all projects",
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false,
         },
-        scales: {
-          x: {
-            grid: {
-              display: false,
-            },
-          },
-          y: {
-            grid: {
-              display: true,
-            },
-          },
+      },
+      y: {
+        grid: {
+          display: true,
         },
-      };
- 
+      },
+    },
+  };
 
   return (
-    <div style={{ width: '100%', minHeight: isXs ? '300px' : '600px' }}>
+    <div style={{ width: "100%", minHeight: isXs ? "300px" : "600px" }}>
       <Bar options={options} data={chartData} />
     </div>
   );
